a2bf2d32d9a7cef3a4334f3b66189e079101b8a9
Zaorong BI A <zaorong.a.bi@ericsson.com>
2020-04-21 16:58:56 +0800

    first add

---
+++ wxcx_xinwen/pages/shouye/shouye.js
@@ -0,0 +1,44 @@
+// pages/shouye/shouye.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    swiperimage:[
+      {src:'/images/timg.jpg'},
+      {src:'/images/timg1.jpg'},
+      {src:'/images/timg2.jpg'}
+    ],
+    newslist:[
+      {id:'',
+        title:'',
+        poster:'',
+        data:''
+     },
+      {id:'',
+      title:'',
+      poster:'',
+      data:''
+    },
+      {id:'',
+      title:'',
+      poster:'',
+      data:''
+     }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
